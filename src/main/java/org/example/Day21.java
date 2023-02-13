2d4c1407dab0f91a52bcb1dc49fb7307d7dda154
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-13 11:18:00 +0900

    01

---
+++ src/main/java/org/example/Day21.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Day21 {
+    public static void main(String[] args) {
+        try {
+            InetAddress addr = InetAddress.getByName("www.inha.ac.kr");
+            //InetAddress addr = InetAddress.getByName("www.kiminha.ac.kr");
+            System.out.println(addr);
+            System.out.println(addr.getCanonicalHostName());
+            System.out.printf("");
+        } catch (UnknownHostException e) {
+            // throw new RuntimeException(e);
+            System.out.println("해당 URL은 존재하지 않습니다.");}
+    }
+}
